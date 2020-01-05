843500f99ee29815831b6478a36d228a41d19049
Elodie Casciola <elodiecasciola@hotmail.com>
2020-01-05 18:27:15 +0100

    Navbar done :)

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include('0-config/config-genos.php'); ?>
 <?php Head('Gabelo Cook Accueil'); ?>
+<?php Menu(); ?>
 	
 <?php Footer(); ?>
--- utilities.php
+++ utilities.php
@@ -11,6 +11,8 @@ function Head($titre){ ?>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php echo RACINE_GLOBAL_RELATIF ?>css/bootstrap.css">
     <link rel="stylesheet" href="<?php echo RACINE_GLOBAL_RELATIF ?>css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo RACINE_GLOBAL_RELATIF ?>css/style.css">
+    <script src="https://kit.fontawesome.com/1b22f83301.js" crossorigin="anonymous"></script>
   </head>
   <body>
 <?php } 
@@ -25,6 +27,32 @@ function Footer() { ?>
 </html>
 <?php }
 
+function Menu(){ ?>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: rgba(106, 196, 112, 0.2);">
+  <a class="navbar-brand navigation" href="#"><i class="fas fa-cookie"></i> Accueil</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Recettes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Générateur de repas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">A propos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+  <a class="navbar-brand navigation" href="#">Ajouter une recette</a>
+  <button class="btn btn-lg navigation"><i class="fas fa-sign-in-alt"></i> Connexion</button>
+</nav>
+<?php }
 
 
 
