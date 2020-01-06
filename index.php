7144fe483d6f2309a094880d80a97d4653ddd58a
Elodie Casciola <elodiecasciola@hotmail.com>
2020-01-06 20:13:05 +0100

    Ajout paramètre fonction Menu

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php include('0-config/config-genos.php'); ?>
-<?php Head('Gabelo Cook Accueil'); ?>
-<?php Menu(); ?>
+<?php Head('Gabelo Cook Accueil', 0); ?>
 	
 <?php Footer(); ?>
--- utilities.php
+++ utilities.php
@@ -1,5 +1,5 @@
 <?php 
-function Head($titre){ ?>
+function Head($titre, $actif){ ?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -15,6 +15,7 @@ function Head($titre){ ?>
     <script src="https://kit.fontawesome.com/1b22f83301.js" crossorigin="anonymous"></script>
   </head>
   <body>
+    <?php Menu($actif); ?>
 <?php } 
 
 function Footer() { ?>
@@ -27,30 +28,30 @@ function Footer() { ?>
 </html>
 <?php }
 
-function Menu(){ ?>
+function Menu($actif){ ?>
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: rgba(106, 196, 112, 0.2);">
-  <a class="navbar-brand navigation" href="#"><i class="fas fa-cookie"></i> Accueil</a>
+  <a class="navbar-brand navigation <?php echo ($actif == 0) ? 'active' : '' ?>" href="#"><i class="fas fa-cookie"></i> Accueil</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
+      <li class="nav-item <?php echo ($actif == 1) ? 'active' : '' ?>">
         <a class="nav-link" href="#">Recettes</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item <?php echo ($actif == 2) ? 'active' : '' ?>">
         <a class="nav-link" href="#">Générateur de repas</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item <?php echo ($actif == 3) ? 'active' : '' ?>">
         <a class="nav-link" href="#">A propos</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item <?php echo ($actif == 4) ? 'active' : '' ?>">
         <a class="nav-link" href="#">Contact</a>
       </li>
     </ul>
   </div>
-  <a class="navbar-brand navigation" href="#">Ajouter une recette</a>
-  <button class="btn btn-lg navigation"><i class="fas fa-sign-in-alt"></i> Connexion</button>
+  <a class="nav-link text-black-50 navigation <?php echo ($actif == 5) ? 'active' : '' ?>" href="#">Ajouter une recette</a>
+  <button class="btn btn-link text-black-50 navigation <?php echo ($actif == 6) ? 'active' : '' ?>"><i class="fas fa-sign-in-alt"></i> Connexion</button>
 </nav>
 <?php }
 
